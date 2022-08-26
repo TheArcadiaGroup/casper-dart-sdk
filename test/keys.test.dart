abedcb308264b8e0e9bf16987d41d5b3c2fb90f7
Faustino Luis <billynelson010694@gmail.com>
2022-08-26 15:28:55 +0700

    update test

--- test/keys.test.dart
+++ test/keys.test.dart
@@ -87,206 +87,41 @@ void main() {
     });
 
     test('should generate PEM file for Secp256k1 correctly', () {
-      // var keyPair = Secp256K1.newKey();
-      // var publicKeyInPem = keyPair.exportPublicKeyInPem();
-      // var privateKeyInPem = keyPair.exportPrivateKeyInPem();
-      var publicKey = Uint8List.fromList([
-        2,
-        249,
-        44,
-        155,
-        121,
-        35,
-        45,
-        179,
-        133,
-        132,
-        173,
-        85,
-        140,
-        245,
-        190,
-        207,
-        91,
-        253,
-        35,
-        152,
-        126,
-        78,
-        29,
-        54,
-        212,
-        145,
-        102,
-        40,
-        158,
-        216,
-        32,
-        143,
-        95
-      ]);
-      var privateKey = Uint8List.fromList([
-        48,
-        116,
-        2,
-        1,
-        1,
-        4,
-        32,
-        87,
-        112,
-        53,
-        29,
-        13,
-        211,
-        248,
-        243,
-        13,
-        170,
-        78,
-        197,
-        76,
-        128,
-        162,
-        6,
-        155,
-        102,
-        138,
-        60,
-        46,
-        183,
-        246,
-        53,
-        200,
-        70,
-        234,
-        102,
-        219,
-        253,
-        198,
-        95,
-        160,
-        7,
-        6,
-        5,
-        43,
-        129,
-        4,
-        0,
-        10,
-        161,
-        68,
-        3,
-        66,
-        0,
-        4,
-        249,
-        44,
-        155,
-        121,
-        35,
-        45,
-        179,
-        133,
-        132,
-        173,
-        85,
-        140,
-        245,
-        190,
-        207,
-        91,
-        253,
-        35,
-        152,
-        126,
-        78,
-        29,
-        54,
-        212,
-        145,
-        102,
-        40,
-        158,
-        216,
-        32,
-        143,
-        95,
-        74,
-        225,
-        253,
-        38,
-        198,
-        79,
-        18,
-        29,
-        230,
-        170,
-        219,
-        234,
-        161,
-        161,
-        234,
-        164,
-        250,
-        208,
-        205,
-        233,
-        118,
-        122,
-        96,
-        27,
-        226,
-        211,
-        90,
-        79,
-        4,
-        100,
-        164,
-        64
-      ]);
-
-      var keyPair = Secp256K1.parseKeyPair(publicKey, privateKey);
-
-      // var tempDir = Directory.systemTemp;
-      // var file1 = File(tempDir.path + '/public.pem');
-      // var file2 = File(tempDir.path + '/private.pem');
-
-      // var publicKeyInPem = keyPair.exportPublicKeyInPem();
+      var keyPair = Secp256K1.newKey();
+      var publicKeyInPem = keyPair.exportPublicKeyInPem();
       var privateKeyInPem = keyPair.exportPrivateKeyInPem();
 
-      print(privateKeyInPem);
+      var tempDir = Directory.systemTemp;
+      var file1 = File(tempDir.path + '/public.pem');
+      var file2 = File(tempDir.path + '/private.pem');
 
-      // file1.writeAsStringSync(publicKeyInPem);
-      // file2.writeAsStringSync(privateKeyInPem);
+      file1.writeAsStringSync(publicKeyInPem);
+      file2.writeAsStringSync(privateKeyInPem);
 
-      // var keyPair2 = Secp256K1.parseKeyFiles(
-      //     tempDir.path + '/public.pem', tempDir.path + '/private.pem');
+      var keyPair2 = Secp256K1.parseKeyFiles(
+          tempDir.path + '/public.pem', tempDir.path + '/private.pem');
 
-      // expect(base64Encode(keyPair.publicKey.value()),
-      //     base64Encode(keyPair2.publicKey.value()));
-      // expect(
-      //     base64Encode(keyPair.privateKey), base64Encode(keyPair2.privateKey));
+      expect(base64Encode(keyPair.publicKey.value()),
+          base64Encode(keyPair2.publicKey.value()));
+      expect(
+          base64Encode(keyPair.privateKey), base64Encode(keyPair2.privateKey));
 
-      // var msg = Uint8List.fromList(('hello world').codeUnits);
-      // var signature = keyPair.sign(msg);
-      // expect(keyPair.verify(signature, msg), true);
+      var msg = Uint8List.fromList(('hello world').codeUnits);
+      var signature = keyPair.sign(msg);
+      expect(keyPair.verify(signature, msg), true);
     });
 
-    // test('get publickey', () {
-    //   var base64_2 = '-----BEGIN EC PRIVATE KEY-----\n'
-    //       'MHQCAQEEIFdwNR0N0/jzDapOxUyAogabZoo8Lrf2NchG6mbb/cZfoAcGBSuBBAAK\n'
-    //       'oUQDQgAE+SybeSMts4WErVWM9b7PW/0jmH5OHTbUkWYontggj19K4f0mxk8SHeaq\n'
-    //       '2+qhoeqk+tDN6XZ6YBvi01pPBGSkQA==\n'
-    //       '-----END EC PRIVATE KEY-----';
-    //   var pk = Secp256K1.readBase64WithPEM(base64_2);
-    //   print(pk);
-    //   var privateKey2 = Secp256K1.parsePrivateKey(pk);
-    //   // print(base16Encode(privateKey2));
-    //   var publicKey2 = Secp256K1.privateToPublicKey(privateKey2);
-    //   print(publicKey2);
-    //   // expect(Secp256K1.accountHexStr(publicKey2),
-    //   //     '02025d0f7d345c9863814ff3ccd934664bbd28fb911d8320b7cab9828f021341705d');
-    // });
+    test('get publickey', () {
+      var base64_2 = '-----BEGIN EC PRIVATE KEY-----\n'
+          'MHQCAQEEIPCR7Cs+AzPFATVPvp/K1zOBQX5ifxfGuCX1kzwy24uXoAcGBSuBBAAK\n'
+          'oUQDQgAEXQ99NFyYY4FP88zZNGZLvSj7kR2DILfKuYKPAhNBcF3ZZgQHUXxT0lb8\n'
+          'teHP8hv36fe9171dQuZZbo7V1Wej8A==\n'
+          '-----END EC PRIVATE KEY-----';
+      var pk = Secp256K1.readBase64WithPEM(base64_2);
+      var privateKey2 = Secp256K1.parsePrivateKey(pk);
+      var publicKey2 = Secp256K1.privateToPublicKey(privateKey2);
+      expect(Secp256K1.accountHexStr(publicKey2),
+          '02025d0f7d345c9863814ff3ccd934664bbd28fb911d8320b7cab9828f021341705d');
+    });
   });
 }
